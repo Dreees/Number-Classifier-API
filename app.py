446a29001c6d7baa625c80a19b7acc90da6c4d4e
Dreees <ayeniidris0@gmail.com>
2025-02-07 17:43:42 +0100

    Fix: Set Flask port to 8080

--- app.py
+++ app.py
@@ -80,5 +80,6 @@ def classify_number():
     return jsonify(response)
 
 # Run the API
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
